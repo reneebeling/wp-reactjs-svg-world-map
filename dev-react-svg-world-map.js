db582bb734e2568607d3a450ab48c274cd9cfbde
René <rene.ebeling@gmx.com>
2020-06-04 18:46:15 +0800

    - changes to enable dev mode server

--- dev-react-svg-world-map.js
+++ dev-react-svg-world-map.js
@@ -2,15 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import WorldLinkMapLazyLoad from './src/components/world-link-lazy-load.jsx';
 
+const rootElmementId = 'reactjs_svg_map';
+
 if (process.env.NODE_ENV !== 'production') {
    console.log('Development mode is activated.');
-   ReactDOM.render(
-        React.createElement('div', {id: 'shopping-list'},
-        React.createElement("h1", null, "React Map DevMode")),
-   );
+   console.log('Root element "%s" will be created.', rootElmementId);  
+   const rootElmement = document.createElement('div');
+   rootElmement.setAttribute("id", rootElmementId);
+   document.body.appendChild(rootElmement);
 }
 
-if ( document.getElementById('reactjs_svg_map') ) {
-    ReactDOM.render(<WorldLinkMapLazyLoad />, document.getElementById('reactjs_svg_map'));
+if ( document.getElementById(rootElmementId) ) {
+    ReactDOM.render(<WorldLinkMapLazyLoad />, document.getElementById(rootElmementId));
     window.onbeforeunload = function () {window.scrollTo(0, 0);}
+}
+else
+{
+    console.log('Root element "%s" does not exist.', rootElmementId);  
 }
